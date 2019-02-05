5663931b5a3593f14c07feba1b393890364dfce5
zaseerr <47345085+zaseerr@users.noreply.github.com>
2019-02-05 12:45:41 +0530

    Update program7.py

--- program7.py
+++ program7.py
@@ -1,1 +1,9 @@
-print('Hello, world!')
+my_string = 'Hello'
+print(my_string)
+my_string = "Hello"
+print(my_string)
+my_string = '''Hello'''
+print(my_string)
+my_string = """Hello, welcome to
+the world of Python"""
+print(my_string)
